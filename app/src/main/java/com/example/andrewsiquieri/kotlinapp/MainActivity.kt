d57e375641378d6411fdc6a849e9da5c216942a9
Andrew <andrew.siquieri@seniorsolution.com.br>
2018-02-23 19:19:42 -0300

    Tentativa de new Intent

--- app/src/main/java/com/example/andrewsiquieri/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/andrewsiquieri/kotlinapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.andrewsiquieri.kotlinapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botao = findViewById < Button >(R.id.ola)
+        botao.setOnClickListener {
+            val main = Intent(this, MainMenu::class.java)
+            startActivity(main)
+        }
+
     }
 }

510b6d72472bbcfe9bf188a2ada083e6385cdb8f
Andrew <andrew.siquieri@seniorsolution.com.br>
2018-03-13 18:04:44 -0300

    Botao de Cadastrar

--- app/src/main/java/com/example/andrewsiquieri/kotlinapp/MainMenu.kt
+++ app/src/main/java/com/example/andrewsiquieri/kotlinapp/MainMenu.kt
@@ -1,9 +1,12 @@
 package com.example.andrewsiquieri.kotlinapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MotionEvent
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.Toast
 import com.example.andrewsiquieri.kotlinapp.ui.adapter.SpinnerAdapter
 import kotlinx.android.synthetic.main.activity_main_menu.*
 
@@ -18,25 +21,34 @@ class MainMenu : AppCompatActivity() {
 
         val listaEstados  = resources.getStringArray(R.array.cadastroPessoalEstados).toMutableList()
 
-        val adapter = SpinnerAdapter(this, android.R.layout.simple_spinner_dropdown_item)
+        val spinnerAdapter = SpinnerAdapter(this, android.R.layout.simple_spinner_dropdown_item)
 
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        adapter.addAll(listaEstados)
-        adapter.add(getString(R.string.cadastroPessoalEstado))
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerAdapter.addAll(listaEstados)
+        spinnerAdapter.add(getString(R.string.cadastroPessoalEstado))
 
-        spinner.adapter = adapter
-        spinner.setSelection(adapter.count) //display hint
+        spinner.adapter = spinnerAdapter
+        spinner.setSelection(spinnerAdapter.count) //display hint
 
         spinner.setOnTouchListener({ _, event ->
             if (event.action == MotionEvent.ACTION_DOWN) {
                 if (spinner.selectedItem.toString() == listaEstados[0]) {
-                    spinner.setSelection(adapter.count)
+                    spinner.setSelection(spinnerAdapter.count)
                 }
                 if (spinner.selectedItem.toString() == getString(R.string.cadastroPessoalEstado)) {
                     spinner.setSelection(0)
                 }
             }
             false
+        })
+
+        val botaoConfirmar: Button = cadastrarButton
+
+        botaoConfirmar.setOnClickListener({
+            val main = Intent(this, MainActivity::class.java)
+            startActivity(main)
+
+            Toast.makeText(this, "Usuário adicionado!", Toast.LENGTH_LONG).show()
         })
 
     }
